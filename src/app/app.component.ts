57fd0619db58b56561cd4c3fd76b8fd4798cd32c
Ing. José Narváez <josedanielnarvaez452@gmail.com>
2021-08-07 11:10:53 -0600

    reactiveForms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Persona } from './models/persona';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tecnasa-ng-ts';
+
+  public formularioPersona: FormGroup = this.fb.group({});
+
+  public personas: Array<Persona> = [];
+  
+  constructor(
+    private fb: FormBuilder  
+  ) {
+    this.formularioPersona = this.crearFormulariPersona();
+  }
+
+  crearFormulariPersona() {
+    return this.fb.group({
+      nombre: [null, [Validators.required]],
+      apellido: [null, [Validators.required]],
+      edad: [null, [Validators.required]],
+    });
+  }
+
+  public agregarPersona() {
+
+    const persona: Persona = this.formularioPersona.value;
+
+    this.personas.push(persona);
+
+  }
+  
+  public eliminarPersona(index: number) {
+    this.personas.splice(index, 1);
+  }
 }
